b47462312bb4cc4706d26ed354b6179b95f11f22
Patrique Legault <patrique.legault@gmail.com>
2024-01-18 13:54:40 -0500

    Adding inital release

--- src/main/java/com/adobe/aem/support/linkrewriter/LinkRewriterImpl.java
+++ src/main/java/com/adobe/aem/support/linkrewriter/LinkRewriterImpl.java
@@ -4,7 +4,12 @@ import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.StringUtils;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.Designate;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xml.sax.Attributes;
@@ -14,29 +19,60 @@ import com.day.cq.rewriter.linkchecker.LinkCheckerSettings;
 import com.day.cq.rewriter.pipeline.RequestRewriter;
 
 @Component(service = RequestRewriter.class)
+@Designate(ocd = LinkRewriterImpl.LinkRewriterConfig.class)
 public class LinkRewriterImpl implements RequestRewriter {
 
+    @ObjectClassDefinition(name = "Adobe - Link Rewriter Config", description = "OSGi Service providing link rewriting capabilities")
+    public @interface LinkRewriterConfig {
+
+        @AttributeDefinition(name = "Regex", description = "Regex that if matches the href will be used to create the outgoing URL")
+        String regex() default StringUtils.EMPTY;
+
+        @AttributeDefinition(name = "Output URL", description = "The output url matching data coming from the regex url, needs to be in format like such \"/$1/dam/$2\"")
+        String out() default StringUtils.EMPTY;
+
+    }
+
     private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private LinkRewriterConfig config;
+
+    @Activate
+    public LinkRewriterImpl(LinkRewriterConfig config) {
+        this.config = config;
+    }
 
     @Override
     public Attributes rewrite(String arg0, Attributes arg1, LinkCheckerSettings arg2) {
         return arg1;
     }
 
+    public boolean hasRegex() {
+        try {
+            Pattern.compile(config.regex());
+            return true && !config.regex().isEmpty();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
     @Override
     public String rewriteLink(Link arg0, LinkCheckerSettings arg1) {
         Optional<String> href = Optional.ofNullable(arg0.getHref());
-        if (!href.isPresent()) {
+        if (!href.isPresent() && !hasRegex()) {
             return null;
         }
-        Pattern pattern = Pattern.compile("\\/content\\/wknd\\/language-masters\\/en\\/(.+)\\/content\\/dam\\/(.+)");
+        Pattern pattern = Pattern.compile(config.regex());
         Matcher matcher = pattern.matcher(href.get());
-        
-        if(!matcher.matches()) {
+
+        if (!matcher.matches()) {
             return null;
         }
-
-        return "/" + matcher.group(1) + "/" + matcher.group(2);
+        int groupSize = matcher.groupCount();
+        String result = config.out();
+        for (int i = 0; i < groupSize; i++) {
+            result = result.replace("$" + (i+1), matcher.group(i +1));
+        }
+        return result;
     }
 
 }
